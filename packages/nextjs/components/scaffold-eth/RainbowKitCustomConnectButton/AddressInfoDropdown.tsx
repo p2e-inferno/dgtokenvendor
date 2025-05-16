09d596f26f4e586e089579fae14dde7bb2b48e83
blahkheart <dannithomx@gmail.com>
2025-05-16 23:06:12 +0100

    add skeleton to basename resolution loading state in connect button

--- packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/AddressInfoDropdown.tsx
+++ packages/nextjs/components/scaffold-eth/RainbowKitCustomConnectButton/AddressInfoDropdown.tsx
@@ -56,11 +56,17 @@ export const AddressInfoDropdown = ({
         <summary tabIndex={0} className="btn btn-secondary btn-sm pl-0 pr-2 shadow-md dropdown-toggle gap-0 !h-auto">
           <BlockieAvatar address={checkSumAddress} size={30} ensImage={ensAvatar} />
           <span className="ml-2 mr-1">
-            {isENS(displayName)
-              ? displayName
-              : baseName && !isBaseNameLoading
-                ? baseName
-                : checkSumAddress?.slice(0, 6) + "..." + checkSumAddress?.slice(-4)}
+            {isBaseNameLoading ? (
+              <div className="ml-1.5 skeleton rounded-lg font-bold animate-pulse">
+                <span className="invisible">0x0123...456</span>
+              </div>
+            ) : isENS(displayName) ? (
+              displayName
+            ) : baseName && !isBaseNameLoading ? (
+              baseName
+            ) : (
+              checkSumAddress?.slice(0, 6) + "..." + checkSumAddress?.slice(-4)
+            )}
           </span>
           <ChevronDownIcon className="h-6 w-4 ml-2 sm:ml-0" />
         </summary>
