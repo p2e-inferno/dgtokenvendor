a3af1ea54b14e8e4d2b6246f76e48afb7fc030bb
blahkheart <dannithomx@gmail.com>
2025-07-29 02:59:01 +0100

    Fix lint warning

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -1,8 +1,8 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
-import "~~/styles/onchainkit.css";
 import "~~/styles/globals.css";
+import "~~/styles/onchainkit.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
